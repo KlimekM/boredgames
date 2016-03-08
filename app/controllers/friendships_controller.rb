ca9bdb61d410b1bda99ab2c75b7e55357ec8341b
tylermckenzie <tyler.mckenzie30@aol.com>
2016-03-08 21:11:22 +0000

    Add user auth for users show pages and add/remove friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,15 @@
 class FriendshipsController < ApplicationController
   def create
-    #add new friend!
+    current_user = User.find(params[:current_user])
+    @user = User.find(params[:id])
+    current_user.friends << @user
+    redirect_to user_path(@user)
   end
 
   def destroy
     #friend breakup
+    current_user = User.find(params[:id])
+    current_user.friends.delete(params[:friend])
+    redirect_to user_path(current_user.id)
   end
 end
