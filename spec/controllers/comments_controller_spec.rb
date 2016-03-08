c994864075a93fdd8c87747732efcc978018aabf
KlimekM <mklimek15@gmail.com>
2016-03-08 17:28:17 -0600

    Create first test for the comments controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,1 +1,9 @@
-comments_controller_spec.rb
+describe CommentsController do
+  let!(:comment) { Comment.create!(text: "Great game!", game_id: 4, user_id: 4) }
+
+describe "GET #new" do
+  it "assigns @comment as a new instance of Comment" do
+    get :new
+    expect(assigns(:comment).to be_a(Comment)
+  end
+end
