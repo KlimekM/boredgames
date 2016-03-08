395335f1aadfffc21849df1cb3f93e4ad8511efc
tylermckenzie <tyler.mckenzie30@aol.com>
2016-03-08 21:26:33 +0000

    Fix two way friendships ;)

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,13 +3,16 @@ class FriendshipsController < ApplicationController
     current_user = User.find(params[:current_user])
     @user = User.find(params[:id])
     current_user.friends << @user
+    @user.friends << current_user
     redirect_to user_path(@user)
   end
 
   def destroy
     #friend breakup
+    friend = User.find(params[:friend])
     current_user = User.find(params[:id])
-    current_user.friends.delete(params[:friend])
-    redirect_to user_path(current_user.id)
+    friend.friends.delete(current_user)
+    current_user.friends.delete(friend)
+    redirect_to user_path(current_user)
   end
 end
